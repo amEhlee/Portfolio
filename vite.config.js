ca593b710fe43301b1ab423f0a5717d668675a3e
AmEhlee <ElieKabengele.Work@gmail.com>
2023-08-31 18:57:13 -0600

    Update Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
+  base: "/",
   plugins: [reactRefresh()],
   server: {
     open: true,
