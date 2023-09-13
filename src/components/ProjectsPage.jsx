0955a435a960d3ebc5052692d6a5b8e65417b777
AmEhlee <ElieKabengele.Work@gmail.com>
2023-09-13 01:37:18 -0600

    Update Projects

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -5,6 +5,8 @@ import { Carousel, Card, Button } from "flowbite-react";
 import PortfolioPage from "../assets/images/PortfolioPage.jpg";
 import CapstonePeralta from "../assets/images/CapstonePeralta.jpg";
 import UnityPlatformer from "../assets/images/UnityPlatformer.jpg";
+import PostalCodeLookup from "../assets/images/PostalCodeLookup.png";
+import UniversityLookup from "../assets/images/UniversityLookup.png";
 
 export function ProjectCard(props) {
   return (
@@ -43,12 +45,15 @@ export default function ProjectsPage() {
           Lay of projects i've done using a variety of technologies and
           resources
         </p>
-        <Carousel slide={false} className="min-w-[400px] sm:min-w-[400px] md:min-w-[550px]">
+        <Carousel
+          slide={false}
+          className="min-w-[400px] sm:min-w-[400px] md:min-w-[550px]"
+        >
           <ProjectCard
             imageSource={CapstonePeralta}
             imageAlt="Capstone Peralta Image"
-            title="Capstone Peralta"
-            description="E-commerce app made for my final year of SAIT for our first client. Fill Carts, place Orders all built using Spring and React"
+            title="Capstone Peralta E-Commerce"
+            description="E-Commerce app made for my final year of SAIT for our first client. Fill Carts, place Orders all built using Spring and React"
             linktype="Github"
             gradientColor="purpleToBlue"
             githubLink="https://github.com/amEhlee/Capstone-Peralta"
@@ -70,6 +75,24 @@ export default function ProjectsPage() {
             linktype="Notion Blog"
             gradientColor="pinkToOrange"
             githubLink="https://lydian-tomato-da3.notion.site/Introduction-To-Unity-ac57c9a1918746e6ae0b10c93e040b32"
+          />
+          <ProjectCard
+            imageSource={UniversityLookup}
+            imageAlt="University Lookup"
+            title="University Lookup"
+            description="Web App built using React and Styled with Tailwind CSS and MUI. Used for looking up universities all accross the world!"
+            linktype="Github"
+            gradientColor="purpleToBlue"
+            githubLink="https://github.com/amEhlee/university-lookup"
+          />
+          <ProjectCard
+            imageSource={PostalCodeLookup}
+            imageAlt="Postal Code Lookup"
+            title="Postal Code Lookup"
+            description="Web App built using React and Styled with Tailwind CSS and MUI. Look up any postal code in the US and view relevant information about it"
+            linktype="Github"
+            gradientColor="purpleToBlue"
+            githubLink="https://github.com/amEhlee/postal-code-lookup"
           />
         </Carousel>
       </section>
